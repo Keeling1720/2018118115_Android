d766ca3b09bfa78bf0805fe13e9ec3c83d2c41b2
Keeling1720 <1098100794@qq.com>
2020-09-23 15:15:33 +0800

    修改layout文件和Activity内容，添加ProgressBar组件

--- BookSource/TextView/app/src/main/java/com/example/textview/MainActivity.java
+++ BookSource/TextView/app/src/main/java/com/example/textview/MainActivity.java
@@ -7,11 +7,13 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private EditText editText;
     private ImageView imageView;
+    private ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
         Button button = (Button) findViewById(R.id.button);
         editText = (EditText) findViewById(R.id.edit_text);
         imageView = (ImageView) findViewById(R.id.image_view);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,7 +33,13 @@ public class MainActivity extends AppCompatActivity {
               //          Toast.makeText(MainActivity.this,inputText,
                 //                Toast.LENGTH_SHORT).show();
                   //      break;
-                        imageView.setImageResource(R.drawable.img_2);
+            //            imageView.setImageResource(R.drawable.img_2);
+              //          break;
+                        int progress = progressBar.getProgress();
+                        progress = progress + 10;
+                        progressBar.setProgress(progress);
+                        break;
+                    default:
                         break;
                 }
             }
