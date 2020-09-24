9ee209c5ef13ba93875ebe031255bd7d158365c2
Keeling1720 <1098100794@qq.com>
2020-09-24 17:31:40 +0800

    自定义标题栏布局，附带图片

--- BookSource/LayoutExample/app/src/main/java/com/example/layoutexample/MainActivity.java
+++ BookSource/LayoutExample/app/src/main/java/com/example/layoutexample/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.layoutexample;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -10,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //隐藏系统自带的标题栏
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.hide();
+        }
     }
 }
