6fff08519a74d53b40842dbbe078d96cf27c9a87
Keeling1720 <1098100794@qq.com>
2020-09-25 22:50:42 +0800

    给自己的ListView界面添加点击事件

--- BookSource/ListViewTest/app/src/main/java/com/example/listviewtest/FruitAdapter.java
+++ BookSource/ListViewTest/app/src/main/java/com/example/listviewtest/FruitAdapter.java
@@ -22,8 +22,12 @@ public class FruitAdapter extends ArrayAdapter<Fruit> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent){
         Fruit fruit = getItem(position);
-        View view = LayoutInflater.from(getContext()).inflate(resourceId, parent,
-                false);
+        View view;
+        if(convertView  == null){
+            view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+        }else {
+            view = convertView;
+        }
         ImageView fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
         TextView fruitName = (TextView) view.findViewById(R.id.fruit_name);
         fruitImage.setImageResource(fruit.getImageId());
