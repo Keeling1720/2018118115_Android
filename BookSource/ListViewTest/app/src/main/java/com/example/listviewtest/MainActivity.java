b2bf729bba89537bdc7183b079c2c8494b1001db
Keeling1720 <1098100794@qq.com>
2020-09-26 11:31:40 +0800

    RecyclerView实现水平布局

--- BookSource/ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
+++ BookSource/ListViewTest/app/src/main/java/com/example/listviewtest/MainActivity.java
@@ -28,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         initFruits();
         RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL);
         recyclerView.setLayoutManager(layoutManager);
         FruitAdapter adapter = new FruitAdapter(fruitList);
         recyclerView.setAdapter(adapter);
