2f72fc1445ab90aff7cb58749d43706b87439957
Keeling1720 <1098100794@qq.com>
2020-09-30 15:07:01 +0800

    实现点击标题栏的Back按钮弹出会话框，选择是则退出应用

--- ThirdHomework/app/src/main/java/com/example/thirdhomework/MainActivity.java
+++ ThirdHomework/app/src/main/java/com/example/thirdhomework/MainActivity.java
@@ -1,11 +1,22 @@
 package com.example.thirdhomework;
 
 import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button backButton;
+    private EditText editText;
+    private  Button edit_Button;
+    private ImageView image;
+    private Button image_Button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +26,50 @@ public class MainActivity extends AppCompatActivity {
         if(actionBar != null){
             actionBar.hide();
         }
+        initWindow();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.title_back:
+                AlertDialog.Builder dialog = new AlertDialog.Builder(MainActivity.this);
+                dialog.setTitle("退出提示");
+                dialog.setMessage("是否确认退出");
+                dialog.setCancelable(false);
+                dialog.setPositiveButton("是", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        System.exit(0);
+                    }
+                });
+                dialog.setNegativeButton("否", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                });
+                dialog.show();
+                break;
+            case R.id.edit_button:
+
+                break;
+            case R.id.image_bt:
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void initWindow(){
+        backButton = findViewById(R.id.title_back);
+        editText = findViewById(R.id.edit_text);
+        edit_Button = findViewById(R.id.edit_button);
+        image = findViewById(R.id.imageView);
+        image_Button = findViewById(R.id.image_bt);
+
+        backButton.setOnClickListener(this);
+        edit_Button.setOnClickListener(this);
+        image_Button.setOnClickListener(this);
     }
 }
