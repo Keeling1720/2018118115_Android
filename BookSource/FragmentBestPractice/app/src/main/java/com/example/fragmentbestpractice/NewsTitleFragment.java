04256eb656fb3b221038c3ecf95cf18da7a87a4b
Keeling1720 <1098100794@qq.com>
2020-10-04 23:10:04 +0800

    加载news_title_frag布局

---
+++ BookSource/FragmentBestPractice/app/src/main/java/com/example/fragmentbestpractice/NewsTitleFragment.java
@@ -0,0 +1,21 @@
+package com.example.fragmentbestpractice;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class NewsTitleFragment extends Fragment {
+    private boolean isTwoPane;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.news_title_frag, container, false);
+        return view;
+    }
+}
