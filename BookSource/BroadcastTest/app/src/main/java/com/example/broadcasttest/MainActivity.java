87d7538f022d774a05c2442f1a71aed991ac6f17
Keeling1720 <1098100794@qq.com>
2020-10-13 14:51:58 +0800

    在MainActivity中定义一个继承BroadcastReceiver的类NetworkChangeRecevier

--- BookSource/BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.java
+++ BookSource/BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.java
@@ -2,7 +2,12 @@ package com.example.broadcasttest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+}
+
+class NetworkChangeReceiver extends BroadcastReceiver{
+    public void onReceive(Context context, Intent intent){
+        Toast.makeText(context, "network changes", Toast.LENGTH_SHORT).show();
     }
 }
