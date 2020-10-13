e4a827753c0bf21f7c70d49d3ddc68fe46d6dbae
Keeling1720 <1098100794@qq.com>
2020-10-13 14:57:01 +0800

    使用IntentFilter监听值为android.net.conn.CONNECTIVITY_CHANGE的action并且使用registerReceiver()对NetworkChangeReceiver和IntentFilter的实例进行注册

--- BookSource/BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.java
+++ BookSource/BroadcastTest/app/src/main/java/com/example/broadcasttest/MainActivity.java
@@ -10,11 +10,17 @@ import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private IntentFilter intentFilter;
+    private NetworkChangeReceiver networkChangeReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        networkChangeReceiver = new NetworkChangeReceiver();
+        registerReceiver(networkChangeReceiver, intentFilter);
     }
 }
 
