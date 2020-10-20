491041f863525a059d513414cb05f181eef19d52
Keeling1720 <1098100794@qq.com>
2020-10-20 15:06:12 +0800

    对出问题后的部分文件进行重新提交

--- ThirdHomework/app/src/main/java/com/example/thirdhomework/MainActivity.java
+++ ThirdHomework/app/src/main/java/com/example/thirdhomework/MainActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
