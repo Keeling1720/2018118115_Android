062f6fd77d9cec4af71cf2a9abcb77df6d36f187
Keeling1720 <1098100794@qq.com>
2020-10-21 15:15:55 +0800

    上传NotificationTest项目

---
+++ BookSource/NotificationTest/app/src/main/java/com/example/notificationtest/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.notificationtest;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button sendNotice;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        sendNotice = (Button) findViewById(R.id.send_notice);
+        sendNotice.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.send_notice:
+                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                Notification notification = new NotificationCompat.Builder(this, "normal")
+                        .setContentTitle("This is content titile")
+                        .setContentText("This is content text")
+                        .setWhen(System.currentTimeMillis())
+                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setLargeIcon(BitmapFactory.decodeResource(getResources(),
+                                R.mipmap.ic_launcher))
+                        .build();
+                manager.notify(1, notification);
+                break;
+            default:
+                break;
+        }
+    }
+}
