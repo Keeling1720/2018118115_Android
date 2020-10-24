1c734d675f4176f83cbba11f9f6c4e294f36bccc
Keeling1720 <1098100794@qq.com>
2020-10-24 23:46:51 +0800

    订正代码及图片，使安卓10.0版本能够正常显示通知

--- BookSource/NotificationTest/app/src/main/java/com/example/notificationtest/MainActivity.java
+++ BookSource/NotificationTest/app/src/main/java/com/example/notificationtest/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 
 import android.app.Notification;
+import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button sendNotice;
+    private NotificationManager manager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,20 +21,25 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         sendNotice = (Button) findViewById(R.id.send_notice);
         sendNotice.setOnClickListener(this);
+        manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel("normal",
+                    "Normal", NotificationManager.IMPORTANCE_DEFAULT);
+            manager.createNotificationChannel(channel);
+        }
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.send_notice:
-                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
                 Notification notification = new NotificationCompat.Builder(this, "normal")
                         .setContentTitle("This is content titile")
                         .setContentText("This is content text")
                         .setWhen(System.currentTimeMillis())
-                        .setSmallIcon(R.mipmap.ic_launcher)
+                        .setSmallIcon(R.drawable.small_icon)
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(),
-                                R.mipmap.ic_launcher))
+                                R.drawable.large_icon))
                         .build();
                 manager.notify(1, notification);
                 break;
