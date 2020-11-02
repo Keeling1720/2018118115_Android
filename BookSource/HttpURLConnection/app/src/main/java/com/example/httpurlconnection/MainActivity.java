3a66d044719d3e109e04eaf162f63da5238cf540
Keeling1720 <1098100794@qq.com>
2020-11-02 17:20:08 +0800

    实现Button的功能，并显示

--- BookSource/HttpURLConnection/app/src/main/java/com/example/httpurlconnection/MainActivity.java
+++ BookSource/HttpURLConnection/app/src/main/java/com/example/httpurlconnection/MainActivity.java
@@ -3,12 +3,83 @@ package com.example.httpurlconnection;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView responseTest;
+    private Button sendRequest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        sendRequest = (Button) findViewById(R.id.send_request);
+        responseTest = (TextView) findViewById(R.id.response_text);
+        sendRequest.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.send_request:
+                sendRequesWithHttpURLConnection();
+        }
+    }
+
+    private void sendRequesWithHttpURLConnection(){
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                HttpURLConnection connection = null;
+                BufferedReader reader = null;
+                try{
+                    URL url = new URL("http://www.baidu.com");
+                    connection = (HttpURLConnection) url.openConnection();
+                    connection.setRequestMethod("GET");
+                    connection.setConnectTimeout(8000);
+                    connection.setReadTimeout(8000);
+                    InputStream in = connection.getInputStream();
+                    //下面对获取到的输入流进行读取
+                    reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder response = new StringBuilder();
+                    String line;
+                    while((line = reader.readLine()) != null){
+                        response.append(line);
+                    }
+                    showResponse(response.toString());
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                    if(reader != null){
+                        try {
+                            reader.close();
+                        }catch (IOException e){
+                            e.printStackTrace();
+                        }
+                    }
+                    if(connection != null){
+                        connection.disconnect();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    private void showResponse(final String response){
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                responseTest.setText(response);
+            }
+        });
     }
 }
