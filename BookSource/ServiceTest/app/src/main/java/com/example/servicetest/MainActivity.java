19376e42d29c484fa5de9b5f1786703d319b1f3b
Keeling1720 <1098100794@qq.com>
2020-11-10 23:21:24 +0800

    实现两个按钮的功能（启动和停止服务）

--- BookSource/ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
+++ BookSource/ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.servicetest;
 
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button startService;
+    private Button stopService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startService = findViewById(R.id.start_service);
+        stopService = findViewById(R.id.stop_service);
+        startService.setOnClickListener(this);
+        stopService.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.start_service:
+                Intent startIntent = new Intent(this, MyService.class);
+                startService(startIntent);
+                break;
+            case R.id.stop_service:
+                Intent stopIntent = new Intent(this, MyService.class);
+                stopService(stopIntent);
+                break;
+            default:
+                break;
+        }
     }
 }
