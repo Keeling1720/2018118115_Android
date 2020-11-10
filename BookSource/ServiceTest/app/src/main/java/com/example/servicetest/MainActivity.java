ea6787af25caeb1b3e69e8fe3a040711a952ece0
Keeling1720 <1098100794@qq.com>
2020-11-10 23:48:52 +0800

    实现绑定与解绑功能

--- BookSource/ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
+++ BookSource/ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
@@ -2,14 +2,34 @@ package com.example.servicetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Service;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
     private Button startService;
     private Button stopService;
+    private Button bindService;
+    private Button unBindService;
+    private MyService.DownloadBinder downloadBinder;
+
+    private ServiceConnection connection = new ServiceConnection() {
+        @Override
+        public void onServiceConnected(ComponentName name, IBinder service) {
+            downloadBinder = (MyService.DownloadBinder) service;
+            downloadBinder.startDownload();
+            downloadBinder.getProgress();
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName name) {
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,8 +37,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         startService = findViewById(R.id.start_service);
         stopService = findViewById(R.id.stop_service);
+        bindService = findViewById(R.id.bind_service);
+        unBindService = findViewById(R.id.unbind_service);
+
         startService.setOnClickListener(this);
         stopService.setOnClickListener(this);
+        bindService.setOnClickListener(this);
+        unBindService.setOnClickListener(this);
     }
 
     @Override
@@ -31,6 +56,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.stop_service:
                 Intent stopIntent = new Intent(this, MyService.class);
                 stopService(stopIntent);
+                break;
+            case R.id.bind_service:
+                Intent bindIntent = new Intent(this, MyService.class);
+                bindService(bindIntent, connection, BIND_AUTO_CREATE);
+                break;
+            case R.id.unbind_service:
+                unbindService(connection);
                 break;
             default:
                 break;
