9020ec36e39b10d42b68b3cd69651e28175e0e2c
Keeling1720 <1098100794@qq.com>
2020-11-10 23:23:59 +0800

    添加Log.d（）方法验证服务是否成功实现启动和停止

--- BookSource/ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
+++ BookSource/ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
@@ -3,6 +3,7 @@ package com.example.servicetest;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class MyService extends Service {
     public MyService(){
@@ -17,15 +18,18 @@ public class MyService extends Service {
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.d("Myservice","销毁活动成功");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("MyService","启动服务成功");
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d("MyService", "创建服务成功");
     }
 }
