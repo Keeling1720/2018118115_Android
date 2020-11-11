cbb836170579f8abf070fb33db7580de5785ec72
Keeling1720 <1098100794@qq.com>
2020-11-11 15:10:09 +0800

    创建AsyncTask的子类DownloadTask，并附上构造函数和该有属性

---
+++ FifthHomework/app/src/main/java/com/example/fifthhomework/DownloadTask.java
@@ -0,0 +1,21 @@
+package com.example.fifthhomework;
+
+import android.os.AsyncTask;
+
+public class DownloadTask extends AsyncTask<String, Integer, Integer> {
+    public static final int TYPE_SUCCESS = 0;
+    public static final int TYPE_FAILED = 1;
+    public static final int TYPE_PAUSED = 2;
+    public static final int TYPE_CANCELED = 3;
+
+    private DownloadListener listener;
+    private boolean isCanceled = false;
+    private boolean isPaused = false;
+    private  int lastProgress;
+
+    public DownloadTask(DownloadListener listener){
+        this.listener = listener;
+    }
+
+
+}
