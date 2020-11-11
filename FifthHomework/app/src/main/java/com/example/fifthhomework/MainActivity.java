06f24cfd591e819ae1418f75b954f534af5cbefe
Keeling1720 <1098100794@qq.com>
2020-11-11 19:46:49 +0800

    启动和绑定服务,并申请WRITE_EXTERNAL_STORAGE权限

--- FifthHomework/app/src/main/java/com/example/fifthhomework/MainActivity.java
+++ FifthHomework/app/src/main/java/com/example/fifthhomework/MainActivity.java
@@ -1,9 +1,15 @@
 package com.example.fifthhomework;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.IntentService;
 import android.content.ComponentName;
+import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.view.View;
@@ -30,6 +36,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initWindow();
+        Intent intent = new Intent(this, DownloadService.class);
+        startService(intent);
+        bindService(intent, connection, BIND_AUTO_CREATE);
+        if(ContextCompat.checkSelfPermission(MainActivity.this, Manifest.
+                permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(MainActivity.this, new
+                    String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        }
     }
 
     private void initWindow(){
