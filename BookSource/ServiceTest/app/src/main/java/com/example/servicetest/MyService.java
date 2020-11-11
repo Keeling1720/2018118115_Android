991e333d720203824c3fe98696eea73d6b04f249
Keeling1720 <1098100794@qq.com>
2020-11-11 09:02:22 +0800

    添加前台服务——一个下载的通知

--- BookSource/ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
+++ BookSource/ServiceTest/app/src/main/java/com/example/servicetest/MyService.java
@@ -1,10 +1,15 @@
 package com.example.servicetest;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
+import android.graphics.BitmapFactory;
 import android.os.Binder;
 import android.os.IBinder;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
 
 public class MyService extends Service {
     private DownloadBinder mBinder = new DownloadBinder();
@@ -45,5 +50,17 @@ public class MyService extends Service {
     public void onCreate() {
         super.onCreate();
         Log.d("MyService", "创建服务成功");
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pi = PendingIntent.getActivity(this, 0, intent, 0);
+        Notification notification = new NotificationCompat.Builder(this, "normal")
+                .setContentTitle("This is content title")
+                .setContentText("This is content text")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.drawable.samll)
+                .setLargeIcon(BitmapFactory.decodeResource(getResources(),
+                        R.drawable.samll))
+                .setContentIntent(pi)
+                .build();
+        startForeground(1, notification);
     }
 }
