498b1d2d67451fa40316d75fe187c741a7e9bf4b
Keeling1720 <1098100794@qq.com>
2020-11-11 19:40:19 +0800

    初始化布局

--- FifthHomework/app/src/main/java/com/example/fifthhomework/MainActivity.java
+++ FifthHomework/app/src/main/java/com/example/fifthhomework/MainActivity.java
@@ -3,12 +3,32 @@ package com.example.fifthhomework;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private Button startDownload;
+    private Button pauseDownload;
+    private Button cancelDownload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initWindow();
+    }
+
+    private void initWindow(){
+        startDownload = findViewById(R.id.start_download);
+        pauseDownload = findViewById(R.id.pause_download);
+        cancelDownload = findViewById(R.id.cancel_download);
+        startDownload.setOnClickListener(this);
+        pauseDownload.setOnClickListener(this);
+        cancelDownload.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
