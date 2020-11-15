e0a25a6ea375e506dddfc2a196ca50d72f033778
Keeling1720 <1098100794@qq.com>
2020-11-15 21:47:11 +0800

    新建继承BroadcastReceiver的子类ForceOfflineReceiver用来动态注册广播接收器

--- Homework_7/app/src/main/java/com/example/homework_7/BaseActivity.java
+++ Homework_7/app/src/main/java/com/example/homework_7/BaseActivity.java
@@ -1,11 +1,17 @@
 package com.example.homework_7;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class BaseActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,5 +22,24 @@ public class BaseActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
         ActivityCollector.removeActivity(this);
+    }
+}
+
+class ForceOfflineReceiver extends BroadcastReceiver{
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle("警告!!!");
+        builder.setMessage("您将被强制下线，请重新登陆");
+        builder.setCancelable(false);
+        builder.setPositiveButton("确认", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                ActivityCollector.finishAll();  //销毁所有活动
+                Intent intent = new Intent(context, LoginActivity.class);
+                context.startActivity(intent);  //重新启动登录活动
+            }
+        });
+        builder.show();
     }
 }
