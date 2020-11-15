557b22be0b3d4c4d0fcac1b3ee4ded6906853de9
Keeling1720 <1098100794@qq.com>
2020-11-15 20:30:55 +0800

    调用系统内置隐式Intent

--- Homework_4/app/src/main/java/com/example/homework_4/MainActivity.java
+++ Homework_4/app/src/main/java/com/example/homework_4/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.IntentService;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -20,7 +21,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                Intent intent = new Intent("com.example.activitytest.ACTION_START");
+                //Intent intent = new Intent("com.example.activitytest.ACTION_START");
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("http://baidu.com"));
                 startActivity(intent);
             }
         });
