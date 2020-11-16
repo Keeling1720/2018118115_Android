70f7b604edde218a14121e857ccdeeaaa97244ba
Keeling1720 <1098100794@qq.com>
2020-11-16 16:58:41 +0800

    初始化活动界面

--- Homework_8/app/src/main/java/com/example/homework_8/MainActivity.java
+++ Homework_8/app/src/main/java/com/example/homework_8/MainActivity.java
@@ -3,12 +3,30 @@ package com.example.homework_8;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button change_text;
+    private TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        change_text = (Button) findViewById(R.id.change_text);
+        text = (TextView) findViewById(R.id.text1);
+        change_text.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.change_text:
+                break;
+            default:
+                break;
+        }
     }
 }
