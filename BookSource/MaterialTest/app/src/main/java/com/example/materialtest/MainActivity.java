28e54cd043cd16de48cee482be743fc1a6363fc1
Keeling1720 <1098100794@qq.com>
2020-11-16 16:43:30 +0800

    使用Toolbar代替ActionBar活动实现

--- BookSource/MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
+++ BookSource/MaterialTest/app/src/main/java/com/example/materialtest/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.materialtest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
