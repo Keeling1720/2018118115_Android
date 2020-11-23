3a0044f37c9460500c35172086e9481ffde2e121
Keeling1720 <1098100794@qq.com>
2020-11-23 10:17:22 +0800

    完善周期函数，观察各代码运行时间

--- Homework_10/app/src/main/java/com/example/homework_10/MyService.java
+++ Homework_10/app/src/main/java/com/example/homework_10/MyService.java
@@ -3,6 +3,7 @@ package com.example.homework_10;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 
@@ -20,15 +21,18 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d("MyService", "onCreate被执行");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("Myservice","onStartCommand被执行");
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.d("MyService", "onDestroy被执行");
     }
 }
