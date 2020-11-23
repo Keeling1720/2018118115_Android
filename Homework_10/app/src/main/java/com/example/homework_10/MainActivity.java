74f4483486b3337e6b9e46693fd052c8f5b6bd94
Keeling1720 <1098100794@qq.com>
2020-11-23 10:10:10 +0800

    初始化界面，并设置按钮的监听器

--- Homework_10/app/src/main/java/com/example/homework_10/MainActivity.java
+++ Homework_10/app/src/main/java/com/example/homework_10/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.homework_10;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button startService;
+    private Button stopService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        startService = findViewById(R.id.start_service);
+        stopService = findViewById(R.id.stop_service);
+        startService.setOnClickListener(this);
+        stopService.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
