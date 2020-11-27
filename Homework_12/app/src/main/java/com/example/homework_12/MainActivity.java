325a679bf0b4448750e1858f4c64e2119301678e
Keeling1720 <1098100794@qq.com>
2020-11-27 23:50:12 +0800

    设置Call为默认选中，并给各菜单项设置了监听器

--- Homework_12/app/src/main/java/com/example/homework_12/MainActivity.java
+++ Homework_12/app/src/main/java/com/example/homework_12/MainActivity.java
@@ -1,23 +1,45 @@
 package com.example.homework_12;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
+import com.google.android.material.navigation.NavigationView;
+
 
 public class MainActivity extends AppCompatActivity {
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        navView = (NavigationView) findViewById(R.id.navView);
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null){
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+        }
+        navView.setCheckedItem(R.id.navCall);
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
     }
 
     public boolean onCreateOptionsMenu(Menu menu){
