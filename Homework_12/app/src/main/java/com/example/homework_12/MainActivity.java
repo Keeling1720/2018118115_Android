fd47bf3018b4e2fff6cd3dc2a5ad3027587d61ce
Keeling1720 <1098100794@qq.com>
2020-11-27 22:10:52 +0800

    实例化ToolBar

--- Homework_12/app/src/main/java/com/example/homework_12/MainActivity.java
+++ Homework_12/app/src/main/java/com/example/homework_12/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.homework_12;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
