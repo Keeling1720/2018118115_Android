309011ef08d8dcd59088a36402b13bb084dda9b2
Keeling1720 <1098100794@qq.com>
2020-12-03 23:25:18 +0800

    修改FruitAdapter，用来实现图片的点击事件

--- Homework_12/app/src/main/java/com/example/homework_12/FruitAdapter.java
+++ Homework_12/app/src/main/java/com/example/homework_12/FruitAdapter.java
@@ -1,6 +1,7 @@
 package com.example.homework_12;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -43,6 +44,18 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> 
         }
         View view = LayoutInflater.from(context).inflate
                 (R.layout.fruit_item, parent, false);
+        final ViewHolder holder = new ViewHolder(view);
+        holder.cardView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                Fruit fruit = fruitList.get(position);
+                Intent intent = new Intent(context, FruitActivity.class);
+                intent.putExtra(FruitActivity.FRUIT_NAME, fruit.getName());
+                intent.putExtra(FruitActivity.FRUIT_IMAGE_ID, fruit.getImageId());
+                context.startActivity(intent);
+            }
+        });
         return new ViewHolder(view);
     }
 
