82c0536c146b42373ce28fc934ba6f037557845c
Keeling1720 <1098100794@qq.com>
2020-12-08 22:19:45 +0800

    给主活动定义各种变量

--- LockWord/app/src/main/java/com/example/lockword/MainActivity.java
+++ LockWord/app/src/main/java/com/example/lockword/MainActivity.java
@@ -3,8 +3,16 @@ package com.example.lockword;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    //用来显示单词和音标
+    private TextView timeText, dateText, wordText, englishText;
+    //用来播放声音
+    private ImageView playVoice;
+    //用来显示时间
+    private String month, day, way, hours, minute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
