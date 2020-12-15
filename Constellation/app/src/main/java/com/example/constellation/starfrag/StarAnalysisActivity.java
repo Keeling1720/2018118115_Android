ff2bd224561b493e553ce21282996fd3402ccb0b
Keeling1720 <1098100794@qq.com>
2020-12-15 21:40:07 +0800

    创建StarAnalysisActivity活动、布局并注册

---
+++ Constellation/app/src/main/java/com/example/constellation/starfrag/StarAnalysisActivity.java
@@ -0,0 +1,16 @@
+package com.example.constellation.starfrag;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.constellation.R;
+
+public class StarAnalysisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_star_analysis);
+    }
+}
