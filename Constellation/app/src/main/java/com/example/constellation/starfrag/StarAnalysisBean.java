378325d5162f5770a1591f90bc2235c98ff20b0f
Keeling1720 <1098100794@qq.com>
2020-12-15 22:55:21 +0800

    创建类StarAnalysisBean

---
+++ Constellation/app/src/main/java/com/example/constellation/starfrag/StarAnalysisBean.java
@@ -0,0 +1,31 @@
+package com.example.constellation.starfrag;
+
+public class StarAnalysisBean {
+    private String title;
+    private String content;
+    private int color;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
+}
