4292952715ff8ed6f62bfdd16449d33f865a5754
Keeling1720 <1098100794@qq.com>
2020-12-16 22:24:21 +0800

    创建活动PartnerAnalysis以及其布局文件并注册

---
+++ Constellation/app/src/main/java/com/example/constellation/partnerfrag/PartnerAnalysisActivity.java
@@ -0,0 +1,16 @@
+package com.example.constellation.partnerfrag;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.constellation.R;
+
+public class PartnerAnalysisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_partner_analysis);
+    }
+}
