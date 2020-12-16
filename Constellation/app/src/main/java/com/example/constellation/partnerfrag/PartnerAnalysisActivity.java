3cb4a7e11675b42e42a9b9b64939fd867ac9404b
Keeling1720 <1098100794@qq.com>
2020-12-16 23:23:20 +0800

    获取上个界面传过来的数据

--- Constellation/app/src/main/java/com/example/constellation/partnerfrag/PartnerAnalysisActivity.java
+++ Constellation/app/src/main/java/com/example/constellation/partnerfrag/PartnerAnalysisActivity.java
@@ -2,6 +2,7 @@ package com.example.constellation.partnerfrag;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -14,12 +15,24 @@ public class PartnerAnalysisActivity extends AppCompatActivity {
     TextView manTv, womanTv, pdTv, vsTv, pfTv, jxTv, zyTv, titleTv;
     CircleImageView manIv, womanIv;
     ImageView backIv;
+    private String man_name, woman_name;
+    private String man_logoname, woman_logoname;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_partner_analysis);
         initView();
+        //获取上个界面传过来的数据
+        getLastData();
+    }
+
+    private void getLastData() {
+        Intent intent = getIntent();
+        man_name = intent.getStringExtra("man_name");
+        woman_name = intent.getStringExtra("woman_name");
+        man_logoname = intent.getStringExtra("man_logoname");
+        woman_logoname = intent.getStringExtra("woman_logoname");
     }
 
     private void initView() {
@@ -33,6 +46,7 @@ public class PartnerAnalysisActivity extends AppCompatActivity {
         jxTv = findViewById(R.id.prtneranalysis_tv_jx);
         zyTv = findViewById(R.id.prtneranalysis_tv_zy);
         titleTv = findViewById(R.id.title_tv);
+        backIv = findViewById(R.id.title_iv_back);
 
     }
 }
