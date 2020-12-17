6854503f1fdb30a19d575d0c850e69e2e69b8d1b
Keeling1720 <1098100794@qq.com>
2020-12-17 22:45:24 +0800

    创建LuckAnalysis活动、布局并注册

---
+++ Constellation/app/src/main/java/com/example/constellation/luckfrag/LuckAnalysisActivity.java
@@ -0,0 +1,16 @@
+package com.example.constellation.luckfrag;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.constellation.R;
+
+public class LuckAnalysisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_luck_analysis);
+    }
+}
