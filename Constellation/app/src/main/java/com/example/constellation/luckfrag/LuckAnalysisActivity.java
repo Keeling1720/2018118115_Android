a86ac30433ec46e4daf7c0073db7b809852ef97d
Keeling1720 <1098100794@qq.com>
2020-12-17 23:08:28 +0800

    初始化控件并设置back键返回上级界面

--- Constellation/app/src/main/java/com/example/constellation/luckfrag/LuckAnalysisActivity.java
+++ Constellation/app/src/main/java/com/example/constellation/luckfrag/LuckAnalysisActivity.java
@@ -4,11 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.example.constellation.R;
 import com.example.constellation.utils.URLContent;
 
-public class LuckAnalysisActivity extends AppCompatActivity {
+public class LuckAnalysisActivity extends AppCompatActivity implements View.OnClickListener {
+    ListView luckLv;
+    TextView nameTv;
+    ImageView backIv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +25,20 @@ public class LuckAnalysisActivity extends AppCompatActivity {
         String name = intent.getStringExtra("name");
         //获取网址
         String luckURL = URLContent.getLuckURL(name);
+        //初始化控件
+        luckLv = findViewById(R.id.luckanalysis_lv);
+        nameTv = findViewById(R.id.title_tv);
+        backIv = findViewById(R.id.title_iv_back);
+        nameTv.setText(name);
+        backIv.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.title_iv_back:
+                finish();
+                break;
+        }
     }
 }
