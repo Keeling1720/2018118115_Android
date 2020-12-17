0787ffc0e5f48ac161ed374d4f3a06442581f91d
Keeling1720 <1098100794@qq.com>
2020-12-17 20:20:49 +0800

    给man和woman使用UTF-8编码

--- Constellation/app/src/main/java/com/example/constellation/utils/URLContent.java
+++ Constellation/app/src/main/java/com/example/constellation/utils/URLContent.java
@@ -1,10 +1,19 @@
 package com.example.constellation.utils;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 public class URLContent {
     //星座配对接口
     public static String getPartnerURL(String men, String women){
         men = men.replace("座","");
         women = women.replace("座","");
+        try {
+            men = URLEncoder.encode(men, "UTF-8");
+            women = URLEncoder.encode(women, "UTF-8");
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
         String url = "http://apis.juhe.cn/xzpd/query?men="+men+"&women="+women+"&key=951ce4468402f012497477f8b9fdeff0";
         return url;
     }
