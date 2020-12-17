2b7e7dc4723dbd066d0069551ab647a5da0723fd
Keeling1720 <1098100794@qq.com>
2020-12-18 07:36:23 +0800

    获取上个界面传过来的数据

--- Constellation/app/src/main/java/com/example/constellation/mefrag/MeFragment.java
+++ Constellation/app/src/main/java/com/example/constellation/mefrag/MeFragment.java
@@ -1,24 +1,38 @@
 package com.example.constellation.mefrag;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 import com.example.constellation.R;
+import com.example.constellation.bean.StarBean;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class MeFragment extends Fragment {
+    CircleImageView iconIv;
+    TextView nameTv;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //获取上个界面的数据
+        Bundle bundle = getArguments();
+        StarBean bean = (StarBean) bundle.getSerializable("info");
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_me, container, false);
+        View view = inflater.inflate(R.layout.fragment_me, container, false);
+        return view;
     }
 }
