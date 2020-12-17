608672846f0a185c093c085c216d244012d6a6b5
Keeling1720 <1098100794@qq.com>
2020-12-18 07:53:36 +0800

    设置适配器

--- Constellation/app/src/main/java/com/example/constellation/mefrag/MeFragment.java
+++ Constellation/app/src/main/java/com/example/constellation/mefrag/MeFragment.java
@@ -1,10 +1,12 @@
 package com.example.constellation.mefrag;
 
+import android.app.Dialog;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -12,8 +14,10 @@ import androidx.fragment.app.Fragment;
 
 import com.example.constellation.R;
 import com.example.constellation.bean.StarBean;
+import com.example.constellation.luckfrag.LuckBaseAdapter;
 import com.example.constellation.utils.AssetsUtils;
 
+import java.util.List;
 import java.util.Map;
 
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -25,12 +29,14 @@ public class MeFragment extends Fragment implements View.OnClickListener{
     CircleImageView iconIv;
     TextView nameTv;
     private Map<String, Bitmap> contentlogoImgMap;
+    private List<StarBean.StarinfoBean> mDatas;
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //获取上个界面的数据
         Bundle bundle = getArguments();
         StarBean bean = (StarBean) bundle.getSerializable("info");
+        mDatas = bean.getStarinfo();
     }
 
     @Override
@@ -51,8 +57,20 @@ public class MeFragment extends Fragment implements View.OnClickListener{
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.mefrag_iv:
-
+                showDialog();
                 break;
         }
+    }
+
+    private void showDialog() {
+        Dialog dialog = new Dialog(getContext());
+        View dialogView = LayoutInflater.from(getContext()).inflate(R.layout.me_dialog, null);
+        dialog.setContentView(dialogView);
+        dialog.setTitle("请选择您的星座");
+        GridView dialogGv = dialogView.findViewById(R.id.mefrag_dialog_gv);
+        //设置适配器
+        LuckBaseAdapter adapter = new LuckBaseAdapter(getContext(), mDatas);
+        dialogGv.setAdapter(adapter);
+
     }
 }
