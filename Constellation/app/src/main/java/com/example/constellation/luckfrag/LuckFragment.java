b0c7da87f682f434c2de9fcc0067fc02300974bc
Keeling1720 <1098100794@qq.com>
2020-12-17 22:11:23 +0800

    获取数据源

--- Constellation/app/src/main/java/com/example/constellation/luckfrag/LuckFragment.java
+++ Constellation/app/src/main/java/com/example/constellation/luckfrag/LuckFragment.java
@@ -7,18 +7,31 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
 
 import com.example.constellation.R;
+import com.example.constellation.bean.StarBean;
+
+import java.io.Serializable;
+import java.util.List;
 
 /**
- * A simple {@link Fragment} subclass.
+ * 运势界面
  */
 public class LuckFragment extends Fragment {
+    GridView luckGv;
+    List<StarBean.StarinfoBean> mDatas;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_luck, container, false);
+        View view = inflater.inflate(R.layout.fragment_luck, container, false);
+        luckGv = view.findViewById(R.id.luckfrag_gv);
+        //获取数据源
+        Bundle bundle = getArguments();
+        StarBean info = (StarBean) bundle.getSerializable("info");
+        mDatas = info.getStarinfo();
+
+        return view;
     }
 }
