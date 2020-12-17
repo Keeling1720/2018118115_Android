29452c9af5b2f828054785b62a6e7f12c6ec71ca
Keeling1720 <1098100794@qq.com>
2020-12-17 20:17:52 +0800

    给men和women赋值

--- Constellation/app/src/main/java/com/example/constellation/utils/URLContent.java
+++ Constellation/app/src/main/java/com/example/constellation/utils/URLContent.java
@@ -3,8 +3,8 @@ package com.example.constellation.utils;
 public class URLContent {
     //星座配对接口
     public static String getPartnerURL(String men, String women){
-        men.replace("座","");
-        women.replace("座","");
+        men = men.replace("座","");
+        women = women.replace("座","");
         String url = "http://apis.juhe.cn/xzpd/query?men="+men+"&women="+women+"&key=951ce4468402f012497477f8b9fdeff0";
         return url;
     }
