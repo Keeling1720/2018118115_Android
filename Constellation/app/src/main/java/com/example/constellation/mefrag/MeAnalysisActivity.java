cd75902ab887c0ae4896a59d7788c1560979deff
Keeling1720 <1098100794@qq.com>
2020-12-18 12:46:12 +0800

    创建MeAnalysis活动、布局并注册

---
+++ Constellation/app/src/main/java/com/example/constellation/mefrag/MeAnalysisActivity.java
@@ -0,0 +1,17 @@
+package com.example.constellation.mefrag;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.constellation.R;
+
+public class MeAnalysisActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_me_analysis);
+
+    }
+}
