f17ab653c574de4076fff2adf37396dd1f992e43
Keeling1720 <1098100794@qq.com>
2020-12-18 09:03:56 +0800

    判断是否是第一次进入，如果是则进入Guide界面，否则进入Main界面

--- Constellation/app/src/main/java/com/example/constellation/home/WelcomeActivity.java
+++ Constellation/app/src/main/java/com/example/constellation/home/WelcomeActivity.java
@@ -1,6 +1,7 @@
 package com.example.constellation.home;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.constellation.MainActivity;
 import com.example.constellation.R;
 
 public class WelcomeActivity extends AppCompatActivity {
@@ -21,7 +23,18 @@ public class WelcomeActivity extends AppCompatActivity {
             if (msg.what == 1){
                 count--;
                 if (count == 0){
-                    Intent intent = new Intent(WelcomeActivity.this, GuideActivity.class);
+                    //判断是否是第一次进入，如果是则进入Guide界面，否则进入Main界面
+                    boolean isFirst = pref.getBoolean("isFirst", true);
+                    Intent intent = new Intent();
+                    if(isFirst){
+                        intent.setClass(WelcomeActivity.this, GuideActivity.class);
+                        //为了下一次不跳转引导界面，更改isFirst值
+                        SharedPreferences.Editor editor = pref.edit();
+                        editor.putBoolean("isFirst", false);
+                        editor.commit();
+                    }else {
+                        intent.setClass(WelcomeActivity.this, MainActivity.class);
+                    }
                     startActivity(intent);
                     finish();
                 }else {
@@ -31,12 +44,15 @@ public class WelcomeActivity extends AppCompatActivity {
             }
         }
     };
+    private SharedPreferences pref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_main);
         tv = findViewById(R.id.welcome_tv);
+        pref = getSharedPreferences("first_pref", MODE_PRIVATE);
+
         handler.sendEmptyMessageDelayed(1, 500);
     }
 }
