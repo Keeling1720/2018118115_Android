7b73b9ccf9961844aa57cef45bcb3a9db34f0793
Keeling1720 <1098100794@qq.com>
2020-12-18 08:42:56 +0800

    创建引导活动，界面并注册

---
+++ Constellation/app/src/main/java/com/example/constellation/home/GuideActivity.java
@@ -0,0 +1,16 @@
+package com.example.constellation.home;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.constellation.R;
+
+public class GuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guide);
+    }
+}
