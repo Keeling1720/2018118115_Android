4c3f52da137cace8917782f64ec28acc5243d0ff
Keeling1720 <1098100794@qq.com>
2020-12-18 12:41:36 +0800

    获取每日运势的JSON文件生成类

---
+++ Constellation/app/src/main/java/com/example/constellation/mefrag/MeBean.java
@@ -0,0 +1,148 @@
+package com.example.constellation.mefrag;
+
+public class MeBean {
+
+    /**
+     * date : 20201218
+     * name : 金牛座
+     * QFriend : 水瓶座
+     * color : 蓝色
+     * datetime : 2020年12月18日
+     * health : 70
+     * love : 60
+     * work : 60
+     * money : 80
+     * number : 6
+     * summary : 金牛座今天愿意去做一些改变，并且认为是必须去做的，这种改变也同时可能来自于外界的变化。今天是一个做自己的日子，将自己的个性表达。
+     * all : 75
+     * resultcode : 200
+     * error_code : 0
+     */
+
+    private int date;
+    private String name;
+    private String QFriend;
+    private String color;
+    private String datetime;
+    private String health;
+    private String love;
+    private String work;
+    private String money;
+    private int number;
+    private String summary;
+    private String all;
+    private String resultcode;
+    private int error_code;
+
+    public int getDate() {
+        return date;
+    }
+
+    public void setDate(int date) {
+        this.date = date;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getQFriend() {
+        return QFriend;
+    }
+
+    public void setQFriend(String QFriend) {
+        this.QFriend = QFriend;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getDatetime() {
+        return datetime;
+    }
+
+    public void setDatetime(String datetime) {
+        this.datetime = datetime;
+    }
+
+    public String getHealth() {
+        return health;
+    }
+
+    public void setHealth(String health) {
+        this.health = health;
+    }
+
+    public String getLove() {
+        return love;
+    }
+
+    public void setLove(String love) {
+        this.love = love;
+    }
+
+    public String getWork() {
+        return work;
+    }
+
+    public void setWork(String work) {
+        this.work = work;
+    }
+
+    public String getMoney() {
+        return money;
+    }
+
+    public void setMoney(String money) {
+        this.money = money;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public String getSummary() {
+        return summary;
+    }
+
+    public void setSummary(String summary) {
+        this.summary = summary;
+    }
+
+    public String getAll() {
+        return all;
+    }
+
+    public void setAll(String all) {
+        this.all = all;
+    }
+
+    public String getResultcode() {
+        return resultcode;
+    }
+
+    public void setResultcode(String resultcode) {
+        this.resultcode = resultcode;
+    }
+
+    public int getError_code() {
+        return error_code;
+    }
+
+    public void setError_code(int error_code) {
+        this.error_code = error_code;
+    }
+}
